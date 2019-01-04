1ffcc2104b5531943fed7cc1f19244ec3f1f8ac1
yuanshaoqian <yuanshaoqiancom@hotmail.com>
2019-01-04 17:23:40 +0800

    first commit

---
+++ newuser.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+for line in `cat ./newuser.txt`;do
+ if [ `grep -c "$line" /etc/passwd` -eq '0' ];then
+  echo "The $user is not exists.Create it...."
+  useradd -m -d /home/$line -g wheel $line
+  usermod -A sshonly $line
+  echo "HBHWcdn#357!" | passwd --stdin $line
+ else
+  echo "The $line is exists,not Create"
+ fi
+done
+
+if [ $? -eq 0 ];then
+ echo "succ new user done."
+ rm ./newuser.txt
+else
+ echo "Error add user error."
+fi
+
+for line in `cat ./newoldusr.txt`;do
+ if [ `grep -c "$line" /etc/passwd` -eq '0' ];then
+  echo "The $user is not exists.not delete"
+ else
+  echo "The $line is exists,delete it."
+  userdel -r $line
+ fi
+done
+
+if [ $? -eq 0 ];then
+ echo "succ del user."
+ rm ./newoldusr.txt
+else
+ echo "error del user."
+fi
+rm $0
+exit 0
